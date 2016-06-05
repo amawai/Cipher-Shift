b2290be5fd8be4028904253d5ee2340e58ee7079
Amanda Wai <amawai@hotmail.com>
2016-06-05 14:45:09 -0400

    Changes for easier readability

--- Cipher_Shift.py
+++ Cipher_Shift.py
@@ -9,23 +9,23 @@ def SecretMessage():
     while (correctInput != True):   #Ensuring the user input for shift_value is actually a number
         try:
             shift_value = int (user_shift_value) 
-            correctInput = True
+            correctInput = True #This is executed if the user's shift value input is valid
         except ValueError:
-            user_shift_value = input("Invalid shift value! Enter a number >>")
+            user_shift_value = input("Invalid shift value! Enter a number >> ")
     
     shift_value = abs(shift_value%26) #Shift value cannot be negative or bigger than 26
     encoded_phrase = ""
     for c in phrase:
         x = ord(c) + shift_value
-        if ord(c) in range(65,91): #for upper-case letters
+        if c in string.ascii_uppercase: #code numbers 65-90 for uppercase 
             if x > 90:
-                x = x%91
+                x = x % 91
                 if x < 65:
                     x += 65 #To return to range[65,90] since modulo yields range[0,90]
             encoded_phrase += chr(x) #use chr(x) to convert to ascii
-        elif ord(c) in range(97,123): #for lower-case letters
+        elif c in string.ascii_lowercase: #code number 97-122 for lowercase
             if x > 122:
-                x = x%123
+                x = x % 123
                 if x < 97:
                     x += 97 #To return to range[97,122] since modulo yields range[0,122]  
             encoded_phrase += chr(x) 
